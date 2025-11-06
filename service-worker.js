23f5c18115f94bb23661a7ca9a2ec1db328e03b4
trivietng-art <trivietng@gmail.com>
2025-11-06 15:58:39 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("mamnon-cache-v1").then(cache => {
+      return cache.addAll(["index.html", "manifest.json", "icon-512.png"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
